380404426f948b47aab75898c346b76c0dc9e885
durranee <durranee@hotmail.com>
2018-02-20 23:34:13 +0000

    failed test for Bike.working? does not return true

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,4 +3,6 @@ require './lib/bike.rb'
 describe Bike do
   # Bike object should respond to working? method
   it { should respond_to(:working?) }
+  # expect Bike.working? to return true
+  it { expect(subject.working?).to be(true) }
 end
