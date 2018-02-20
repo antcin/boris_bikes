67a8be570aae55c799758bf0e96fb39f80f4bef2
durranee <durranee@hotmail.com>
2018-02-20 18:14:47 +0000

    alternative one-liners done and passed

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -12,5 +12,6 @@ describe DockingStation do
 #  end
 
 # it { is_expected.to respond_to(:release_bike)}
-it { should respond_to(:release_bike)}
+ it { should respond_to(:release_bike)} #alternative one liner
+
 end
